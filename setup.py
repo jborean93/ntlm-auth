e4ffeb543384e5f32769614f4ec875fffcb46aef
BOREAN, Jordan <Jordan.BOREAN@suncorp.com.au>
2016-08-29 08:54:41 +1000

    Tidied up the setup.py file

--- setup.py
+++ setup.py
@@ -17,9 +17,11 @@ setup(
         with all the latest standards such as NTLMv2, Extended Protection
         (CBT), message integrity and confidentiality (signing and sealing).
     """,
+    keywords='authentication auth microsoft ntlm lm',
     license='GNU Lesser GPL',
     classifiers=[
-        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
