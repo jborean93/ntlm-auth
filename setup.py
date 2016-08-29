7210f3f7df413fb398697688b09dae66a69afb22
jborean93 <jborean93@gmail.com>
2016-08-29 10:11:38 +1000

    Bumped version due to broken pypi upload on 1.0.0 (#3)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='ntlm-auth',
-    version='1.0.0',
+    version='1.0.1',
     packages=[ 'ntlm_auth' ],
     install_requires=[ 'six' ],
     author='Jordan Borean',
