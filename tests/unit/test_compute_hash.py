ff28030cbf983a67f9363fcb849a0ec2e7a2ebbe
nnam <nicholasnam@gmail.com>
2017-06-12 21:03:07 -0700

    Added additional unit tests for compute_hash

--- tests/unit/test_compute_hash.py
+++ tests/unit/test_compute_hash.py
@@ -27,3 +27,19 @@ class Test_ComputeHash(unittest.TestCase):
         actual = comphash._ntowfv2(user_name, password, domain_name)
 
         assert actual == expected
+
+    # 4.2.2.1.1 - LMOWFv1()
+    def test_lmofv1_hash(self):
+        expected = ntlmv1_lmowfv1
+
+        actual = comphash._lmowfv1(password_hash)
+
+        assert actual == expected
+
+    # 4.2.2.1.2 - NTOWFv1()
+    def test_ntofv1_hash(self):
+        expected = ntlmv1_ntowfv1
+
+        actual = comphash._ntofv2(user_name, password_hash, domain_name)
+
+        assert actual == expected
